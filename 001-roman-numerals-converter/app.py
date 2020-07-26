71d974c63bb9ac489917f057fc1d25736a2b3945
bluebay <bluebayspring@gmail.com>
2020-07-26 18:03:19 +0300

    004-phonebook-web-application

--- 001-roman-numerals-converter/app.py
+++ 001-roman-numerals-converter/app.py
@@ -21,5 +21,5 @@ def main_post():
         return render_template('index.html', dpythoneveloper_name='Spring', not_valid=True)
     return render_template('result.html', developer_name='Spring', number_decimal=number, number_roman=convert(number))
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
