1ea7a3387e87c33a6a32c815b2835c40c76d484a
bluebay <bluebayspring@gmail.com>
2020-09-23 15:52:04 +0300

    001-roman

--- projects/001-roman-numerals-converter/app.py
+++ projects/001-roman-numerals-converter/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 def convert(decimal_number):
     roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
         50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
@@ -17,9 +18,9 @@ def main_post():
     if not alpha.isdecimal():
         return render_template('index.html', developer_name='Spring', not_valid=True)
     number=int(alpha)
-    if not (0 < number < 4000):
-        return render_template('index.html', dpythoneveloper_name='Spring', not_valid=True)
-    return render_template('result.html', developer_name='Spring', number_decimal=number, number_roman=convert(number))
+    if not (0 < aaa < 4000):
+        return render_template('index.html', developer_name='Spring', not_valid=True)
+    return render_template('index.html', number_decimal=number, not_valid=True, number_roman=convert(number))
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
